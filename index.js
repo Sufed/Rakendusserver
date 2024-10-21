ccfdc7393576d6cfdefc055a6a25e55ecc4b73df
artemi <artemi.mihhalenkov@gmail.com>
2024-10-21 10:59:27 +0000

    Первоначальный commit: добавлен readme.md

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Главный маршрут
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Динамическое приветствие
+app.get('/hello', (req, res) => {
+    const name = req.query.name || 'World';
+    res.send(`Hello, ${name}`);
+});
+
+// Запуск сервера
+app.listen(port, () => {
+    console.log(`Приложение слушает на http://localhost:${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Hello World Node.js Application
+
+Это приложение на Node.js, которое использует Express и отображает динамическое приветствие.
